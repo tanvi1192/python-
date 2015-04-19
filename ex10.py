d2f18059c6061384359285ed4f97b954cc61e51e
tanvi1192 <tanvihumane@gmail.com>
2015-04-19 20:39:46 +0530

    First commit

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in." # \t writes as a paragraph. t is a tab character
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat." # ???
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+''' # \t starts the string as a paragraph i.e. leaves some space then starts string.
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat 
